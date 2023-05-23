f04be301d5525bc43e0b8eb17ac20fde40aa441f
BogdanAziev2003 <neazievbogdan@gmail.com>
2023-05-23 17:50:41 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Совместимость имен" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="result-block">
+        <asp:Label CssClass="result" ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+
+
+    <div class="input-list">
+
+
+        <asp:TextBox CssClass="" ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </div>
+
+
+    <asp:Button CssClass="btn" ID="Button1" runat="server" Text="Посмотреть результат" OnClick="Button1_Click1" />
+
+        </asp:Content>
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            string name1 = TextBox1.Text;
+            string name2 = TextBox2.Text;
+
+            int sum = 0;
+            foreach (char s in name1)
+            {
+                sum += s - '0';
+            }
+            foreach (char s in name2)
+            {
+                sum += s - '0';
+            }
+
+            Label1.Text = (sum % 100).ToString() + "%";
+        }
+    }
+}
